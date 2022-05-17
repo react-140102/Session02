db7f47fd7917ef578cc2dc40ad28e13a3f8b71f1
pashaie <pashaie@users.noreply.github.com>
2022-05-17 21:12:55 +0430

    a

--- src/Component/Posts.tsx
+++ src/Component/Posts.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function () {
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [totalPosts, setTotalPosts] = useState(0);
 
   /***
    * Ajax:
@@ -36,17 +38,51 @@ export default function () {
 
     //IIFE
     // (async () => {})()
+    // (async () => {
+    //   const resp = await fetch(
+    //     "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=1"
+    //   );
+    //   setPosts(await resp.json());
+    // })();
+
     (async () => {
-      const resp = await fetch(
+      const resp = await axios.get<Post[]>(
         "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=1"
       );
-      setPosts(await resp.json());
+      // setTotalPosts(parseInt(resp.headers["x-total-count"]));
+      setTotalPosts(+resp.headers["x-total-count"]); //conver string to number
+      setPosts(resp.data);
     })();
   }, []);
 
   return (
     <>
       <div>Posts {posts.length}</div>
+      <table className="table table-striped">
+        <thead>
+          <th scope="col">id</th>
+          <th scope="col">userId</th>
+          <th scope="col">title</th>
+          <th scope="col">body</th>
+        </thead>
+        <tbody>
+          {posts.map((post) => (
+            <tr>
+              <td>{post.id}</td>
+              <td>{post.userId}</td>
+              <td>{post.title}</td>
+              <td>{post.body.substring(0, 30)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
+}
+
+interface Post {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
 }
