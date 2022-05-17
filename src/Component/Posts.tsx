295f04d12ecfada99a779e8e82986efc607224a1
pashaie <pashaie@users.noreply.github.com>
2022-05-17 21:31:20 +0430

    Component/Posts:

--- src/Component/Posts.tsx
+++ src/Component/Posts.tsx
@@ -4,6 +4,8 @@ import axios from "axios";
 export default function () {
   const [posts, setPosts] = useState<Post[]>([]);
   const [totalPosts, setTotalPosts] = useState(0);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
 
   /***
    * Ajax:
@@ -46,18 +48,54 @@ export default function () {
     // })();
 
     (async () => {
+      setLoading(true);
       const resp = await axios.get<Post[]>(
-        "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=1"
+        "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=" + page
       );
       // setTotalPosts(parseInt(resp.headers["x-total-count"]));
       setTotalPosts(+resp.headers["x-total-count"]); //conver string to number
       setPosts(resp.data);
+      setLoading(false);
     })();
-  }, []);
+  }, [page]);
+
+  function renderPages() {
+    let pages = [];
+    for (let i = 1; i <= totalPosts / 10; i++) {
+      pages.push(
+        <>
+          <li className={i === page ? "page-item active" : "page-item"}>
+            <a className="page-link" href="#" onClick={() => setPage(i)}>
+              {i}
+            </a>
+          </li>
+        </>
+      );
+    }
+    return pages;
+  }
 
   return (
     <>
       <div>Posts {posts.length}</div>
+      <nav aria-label="Page navigation example">
+        <ul className="pagination">
+          <li className="page-item">
+            <a className="page-link" href="#">
+              Previous
+            </a>
+          </li>
+
+          {renderPages()}
+
+          <li className="page-item">
+            <a className="page-link" href="#">
+              Next
+            </a>
+          </li>
+        </ul>
+      </nav>
+
       <table className="table table-striped">
         <thead>
           <th scope="col">id</th>
@@ -66,6 +104,11 @@ export default function () {
           <th scope="col">body</th>
         </thead>
         <tbody>
+          {loading && (
+            <tr>
+              <td colSpan={10}>Loading</td>
+            </tr>
+          )}
           {posts.map((post) => (
             <tr>
               <td>{post.id}</td>
