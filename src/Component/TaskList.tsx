3203f2a62fd351902db1df98ce0f84b47b7779ee
pashaie <pashaie@users.noreply.github.com>
2022-05-17 18:05:15 +0430

    Component/TaskList:

--- src/Component/TaskList.tsx
+++ src/Component/TaskList.tsx
@@ -1,8 +1,32 @@
+import { useState } from "react";
+
+interface Task {
+  id: number;
+  title: string;
+  done: boolean;
+}
+
 export default function () {
+  const [tasks, setTasks] = useState<Task[]>([
+    { id: 1, title: "React hooks", done: true },
+    { id: 1, title: "Ajax", done: false },
+  ]);
+
+  const toggleTask = (task: Task) => {
+    task.done = !task.done;
+    setTasks([...tasks]);
+    // setTasks(tasks.slice());
+  };
+
   return (
     <>
       <ul>
-        <li>Task</li>
+        {tasks.map((task) => (
+          <li>
+            <input type="checkbox" disabled checked={task.done} />
+            <a onClick={() => toggleTask(task)}>{task.title}</a>
+          </li>
+        ))}
       </ul>
     </>
   );
