6a2f44b1b180b0390161a24bff1531059d382deb
pashaie <pashaie@users.noreply.github.com>
2022-05-17 18:17:44 +0430

    Component/TaskList:

--- src/Component/TaskList.tsx
+++ src/Component/TaskList.tsx
@@ -6,7 +6,15 @@ interface Task {
   done: boolean;
 }
 
+//Before Hooks
+//Class
+//Dumb Component
+
+//React Hook
+//
+
 export default function () {
+  console.log("TaskList " + Math.random());
   const [tasks, setTasks] = useState<Task[]>([
     { id: 1, title: "React hooks", done: true },
     { id: 1, title: "Ajax", done: false },
