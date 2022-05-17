97345bcfa0a18dbe101b7bed3a3115190ce12048
pashaie <pashaie@users.noreply.github.com>
2022-05-17 20:33:48 +0430

    src:

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Posts from "./Component/Posts";
 import TaskList from "./Component/TaskList";
 
 function App() {
+  const [showPosts, setShowPosts] = useState(true);
   return (
     <div className="container">
-      <Posts></Posts>
+      <button
+        onClick={() => setShowPosts(!showPosts)}
+        className="btn btn-primary"
+      >
+        Toggle Posts
+      </button>
+      {showPosts && <Posts></Posts>}
       <TaskList></TaskList>
     </div>
   );
--- src/Component/Posts.tsx
+++ src/Component/Posts.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 export default function () {
   /***
    * Ajax:
@@ -7,9 +9,35 @@ export default function () {
    *    got, ...
    */
 
-  fetch("https://jsonplaceholder.typicode.com/posts?_limit=10&_page=2")
-    .then((resp) => resp.json())
-    .then((json) => console.log(json));
+  useEffect(() => {
+    //Promise
+    // fetch("https://jsonplaceholder.typicode.com/posts?_limit=10&_page=2")
+    //   .then((resp) => resp.json())
+    //   .then((json) => console.log(json));
+
+    // async function run() {
+    //   //async / await
+    //   const resp = await fetch(
+    //     "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=2"
+    //   );
+    //   const json = await resp.json();
+    // }
+    // run();
+
+    //IIFE
+    // (async () => {})()
+    (async () => {
+      const resp = await fetch(
+        "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=2"
+      );
+      const json = await resp.json();
+    })();
+
+    return () => {
+      //Cleanup
+      console.log("Goodbye");
+    };
+  });
 
   return (
     <>
